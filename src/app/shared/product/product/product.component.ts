3b4b62f9917452a8c1eda60abd7002a099857c7b
Mario Alejandro Reyes Medina <73566712+AlejandroReyes666@users.noreply.github.com>
2025-09-02 15:22:16 -0500

    se le dio formato al product

--- src/app/shared/product/product/product.component.ts
+++ src/app/shared/product/product/product.component.ts
@@ -1,7 +1,14 @@
-import { Component,Input,Output,EventEmitter, OnInit, output } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  output,
+} from '@angular/core';
 import { Producto } from '../../../core/models/ProductosModel';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { DialogEliminarComponent } from '../../../shared/dialogEliminar/dialog-eliminar/dialog-eliminar.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
@@ -11,81 +18,81 @@ import { CartService } from '../../../core/service/cart.service';
 import { CartItem } from '../../../core/models/cartItemsModel';
 import { Observable } from 'rxjs';
 
-
 @Component({
   selector: 'app-product',
-  standalone:true,
-  imports: [MatCardModule, MatButtonModule, DialogEliminarComponent, MatIcon, CommonModule],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    DialogEliminarComponent,
+    MatIcon,
+    CommonModule,
+  ],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.scss'
+  styleUrl: './product.component.scss',
 })
 export class ProductComponent implements OnInit {
-  @Input() Product!:Producto
+  @Input() Product!: Producto;
   @Output() editar = new EventEmitter<Producto>();
-  @Output() eliminar= new EventEmitter<number>();
+  @Output() eliminar = new EventEmitter<number>();
   @Output() addToCartEvent = new EventEmitter<Producto>();
-  
-  cartItems$!: Observable<CartItem[]>; 
+
+  cartItems$!: Observable<CartItem[]>;
 
   isAdmin: boolean = false;
 
-  constructor( 
-    private dialog: MatDialog, private AuthService: AuthService,
-  private cartService: CartService
-  ) {  }
+  constructor(
+    private dialog: MatDialog,
+    private AuthService: AuthService,
+    private cartService: CartService
+  ) {}
 
-  actualizarProducto(){
-    console.log("El producto a actializar es ", this.Product);
+  actualizarProducto() {
+    console.log('El producto a actializar es ', this.Product);
     this.editar.emit(this.Product);
   }
 
+  openDialogEliminar(product: Producto): void {
+    const dialogRef = this.dialog.open(DialogEliminarComponent, {
+      width: '550px',
+      data: product || null,
+    });
 
- openDialogEliminar(product:Producto):void{
-  const dialogRef = this.dialog.open(DialogEliminarComponent, {
-    width: '550px',
-    data: product || null
-    
-  });
-
-  dialogRef.afterClosed().subscribe((productoIdEliminado: number | undefined) => {
-    if (productoIdEliminado !== undefined) {
-      this.eliminar.emit(productoIdEliminado);
-    }
-  });
-
-}
+    dialogRef
+      .afterClosed()
+      .subscribe((productoIdEliminado: number | undefined) => {
+        if (productoIdEliminado !== undefined) {
+          this.eliminar.emit(productoIdEliminado);
+        }
+      });
+  }
 
   ngOnInit(): void {
     this.cartItems$ = this.cartService.cartItems$;
-    this.AuthService.role$.subscribe(role => {
+    this.AuthService.role$.subscribe((role) => {
       this.isAdmin = role === 'administrador';
     });
   }
 
   addToCart(product: Producto): void {
-    console.log("Producto añadido al carrito:", product);
+    console.log('Producto añadido al carrito:', product);
     this.cartService.addToCart(product);
     this.addToCartEvent.emit(product);
-    
   }
 
-   removeItem(productId: number): void {
+  removeItem(productId: number): void {
     this.cartService.removeFromCart(productId);
-    
   }
 
   removeOne(product: Producto) {
-  this.cartService.removeOneFromCart(product);
-}
-
+    this.cartService.removeOneFromCart(product);
+  }
 
   isProductInCart(items: CartItem[], productId: number): boolean {
-  console.log("resultado en isProductInCart: ", items.some(item => item.product.id === productId));
-  return items.some(item => item.product.id === productId);
+    console.log(
+      'resultado en isProductInCart: ',
+      items.some((item) => item.product.id === productId)
+    );
+    return items.some((item) => item.product.id === productId);
+  }
 }
-
-
-
-
-}
-
