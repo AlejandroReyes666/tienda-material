bc882c06a8eaf6d2ad319aa57a8ba7c11d11ea67
Mario Alejandro Reyes Medina <73566712+AlejandroReyes666@users.noreply.github.com>
2025-10-20 10:41:06 -0500

    feat(contact): mejoras visuales, responsividad y propuesta de enriquecimiento
    - Corrección de estilos en formulario de contacto: paddings, íconos, sombras y estructura
    - Implementación de animación de entrada suave para mejorar experiencia de usuario
    - Estilos encapsulados en el componente <app-contact-form> con fondo emocional y responsivo
    - Propuesta visual para enriquecer sección con información directa y beneficios laborales
    - Preparación para implementación de mapa, contacto directo y narrativa visual

--- src/app/features/aboutUs/about-us/about-us.component.ts
+++ src/app/features/aboutUs/about-us/about-us.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import {MatDivider} from "@angular/material/divider";
 
 
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatDivider],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
   animations: [
